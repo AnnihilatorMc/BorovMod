790986272f130548f67ea17f37182ba5abe7a9ef
AnnihilatorMc <ladika.bor@gmail.com>
2022-11-01 20:13:28 +0100

    commit

--- src/main/java/net/bor/borovmod/item/ModItems.java
+++ src/main/java/net/bor/borovmod/item/ModItems.java
@@ -13,8 +13,18 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, BorovMod.MOD_ID);
 
+
     public static final RegistryObject<Item> SARMA = ITEMS.register("sarma",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(12).saturationMod(16).build())));
+
+    public static final RegistryObject<Item> SUROVA_SARMA = ITEMS.register("surova_sarma",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(3).saturationMod(2).build())));
+
+    public static final RegistryObject<Item> ZELJE = ITEMS.register("zelje",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(2).saturationMod(2).build())));
+
+    public static final RegistryObject<Item> KAMEN_MODROSTI = ITEMS.register("kamen_modrosti",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
