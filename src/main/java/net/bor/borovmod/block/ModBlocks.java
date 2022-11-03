0b49b19c9877326cd2fba2e036a0d1a994b37e56
AnnihilatorMc <ladika.bor@gmail.com>
2022-11-03 15:59:04 +0100

    Fixed wood and added loot tables

--- src/main/java/net/bor/borovmod/block/ModBlocks.java
+++ src/main/java/net/bor/borovmod/block/ModBlocks.java
@@ -8,6 +8,8 @@ import net.bor.borovmod.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -16,6 +18,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
 
@@ -36,28 +39,48 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
                     .strength(7f).requiresCorrectToolForDrops()), ModCreativeModeTab.BALKAN_TAB);
 
-    public static final RegistryObject<Block> OLIVE_LOG = registerBlock("olive_log",
+    public static final RegistryObject<Block> OLIVE_LOG = registerBlockThatIsAlsoFuel("olive_log",
             () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)),
-            ModCreativeModeTab.BALKAN_TAB);
+            ModCreativeModeTab.BALKAN_TAB, 300);
 
-    public static final RegistryObject<Block> STRIPPED_OLIVE_LOG = registerBlock("stripped_olive_log",
+    public static final RegistryObject<Block> STRIPPED_OLIVE_LOG = registerBlockThatIsAlsoFuel("stripped_olive_log",
             () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG)),
-            ModCreativeModeTab.BALKAN_TAB);
+            ModCreativeModeTab.BALKAN_TAB, 300);
 
-    public static final RegistryObject<Block> OLIVE_WOOD = registerBlock("olive_wood",
+    public static final RegistryObject<Block> OLIVE_WOOD = registerBlockThatIsAlsoFuel("olive_wood",
             () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)),
-            ModCreativeModeTab.BALKAN_TAB);
+            ModCreativeModeTab.BALKAN_TAB, 300);
 
-    public static final RegistryObject<Block> STRIPPED_OLIVE_WOOD = registerBlock("stripped_olive_wood",
+    public static final RegistryObject<Block> STRIPPED_OLIVE_WOOD = registerBlockThatIsAlsoFuel("stripped_olive_wood",
             () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_WOOD)),
-            ModCreativeModeTab.BALKAN_TAB);
+            ModCreativeModeTab.BALKAN_TAB, 300);
 
-    public static final RegistryObject<Block> OLIVE_PLANKS = registerBlock("olive_planks",
+    public static final RegistryObject<Block> OLIVE_PLANKS = registerBlockThatIsAlsoFuel("olive_planks",
             () -> new ModPlanks(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)),
-            ModCreativeModeTab.BALKAN_TAB);
+            ModCreativeModeTab.BALKAN_TAB, 300);
 
 
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+    private static <T extends Block> RegistryObject<T> registerBlockThatIsAlsoFuel(String name, Supplier<T> block,
+                                                                                        CreativeModeTab tab, int burnTime) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItemThatIsAlsoFuel(name, toReturn, tab, burnTime);
+        return toReturn;
+    }
+
+    private static <T extends Block>RegistryObject<Item> registerBlockItemThatIsAlsoFuel(String name, RegistryObject<T> Block,
+                                                                           CreativeModeTab tab, int burnTime) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(Block.get(),
+                new Item.Properties().tab(tab)) {
+            @Override
+            public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
+                return burnTime;
+            }
+        });
+    }
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block,
+                                                                     CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn, tab);
         return toReturn;
